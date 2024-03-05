978daf3a9af22f8c876b7b6320ebcd3e45311c0c
adamZeus <ademlolplayer@gmail.com>
2024-03-05 15:50:08 +0100

    Add files via upload

---
+++ ten_queens_puzzle.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ten_queens_puzzle.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adjemaa <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/08/03 23:42:47 by adjemaa           #+#    #+#             */
+/*   Updated: 2019/08/05 17:54:14 by adjemaa          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putnbr(char c)
+{
+	write(1, &c, 1);
+}
+
+int		print_queens(int board[10][10])
+{
+	int			i;
+	int			j;
+	static int	total;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			if (board[i][j] == 1)
+				ft_putnbr(j + '0');
+			j++;
+		}
+		i++;
+	}
+	total++;
+	write(1, "\n", 1);
+	return (total);
+}
+
+
+
+int		find_solution(int tab[10][10], int col)
+{
+	int i;
+
+	i = 0;
+	if (col == 10)
+	{
+		total = print_queens(tab);
+		return (1);
+	}
+	
+
+}
+
+int		ft_ten_queens_puzzle(void)
+{
+	int tab[10][10];
+	int i;
+	int j;
+
+	i = 0;
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 10)
+		{
+			tab[i][j] = 0;
+			j++;
+		}
+		i++;
+	}
+	return (find_solution(tab, 0));
+}
+
+int main()
+{
+	ft_ten_queerns_puzzle();
+	return (0);
+}
